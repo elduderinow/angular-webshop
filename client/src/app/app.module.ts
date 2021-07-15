f185c3b360e27f1f91bd8ea2a97f02fd928f7474
elduderinow <yarrut.franken@gmail.com>
2021-07-15 15:20:37 +0200

    boilerplate finished

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './src/components/header/header.component';
 import { MainComponent } from './src/components/main/main.component';
 import { FooterComponent } from './src/components/footer/footer.component';
+import { NavbarComponent } from './src/components/main/navbar/navbar.component';
+import { OrdersComponent } from './src/components/main/orders/orders.component';
+import { ProductsComponent } from './src/components/main/products/products.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarComponent,
+    OrdersComponent,
+    ProductsComponent,
+
   ],
   imports: [
     BrowserModule,
