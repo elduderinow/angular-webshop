e899c2c636dffdd1ffa7615484ade213445cb43f
Tom Dirinck <tom.dirinck@gmail.com>
2021-07-15 15:56:08 +0200

    Fix commit.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrdersComponent } from './src/components/main/orders/orders.component';
+import { ProductsComponent } from './src/components/main/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'orders', component: OrdersComponent },
+  { path: 'products', component: ProductsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
