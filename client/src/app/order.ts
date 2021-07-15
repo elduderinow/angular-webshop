884ca55937676bbebdebaf3a8d5c19b1174bb13b
Tom Dirinck <tom.dirinck@gmail.com>
2021-07-15 17:05:49 +0200

    Filled in models.

--- client/src/app/order.ts
+++ client/src/app/order.ts
@@ -1,2 +1,13 @@
 export class Order {
+  name: string;
+  count: number;
+  date: string;
+  customer: string;
+
+  constructor(name: string, count: number, date: string, customer: string) {
+    this.name = name;
+    this.count = count;
+    this.date = date;
+    this.customer = customer;
+  }
 }
--- client/src/app/product.ts
+++ client/src/app/product.ts
@@ -1,2 +1,13 @@
 export class Product {
+  name: string;
+  stock: number;
+  description: string;
+  price: number;
+
+  constructor(name: string, stock: number, description: string, price: number) {
+    this.name = name;
+    this.stock = stock;
+    this.description = description;
+    this.price = price;
+  }
 }
