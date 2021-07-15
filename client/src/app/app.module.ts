9483c70e81bdaafb3ca163f23d5ea788e06bfca6
elduderinow <yarrut.franken@gmail.com>
2021-07-15 11:28:01 +0200

    installed tailwind css and created components

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './src/components/header/header.component';
+import { MainComponent } from './src/components/main/main.component';
+import { FooterComponent } from './src/components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
