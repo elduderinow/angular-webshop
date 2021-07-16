54481f3026ea9be45d923681128fded8cd6c4207
elduderinow <yarrut.franken@gmail.com>
2021-07-16 14:40:15 +0200

    fixed styling responsive and created footer

--- client/src/app/src/components/main/new-product/add-product.service.ts
+++ client/src/app/src/components/main/new-product/add-product.service.ts
@@ -6,11 +6,9 @@ import {NewProduct} from "./new-product";
   providedIn: 'root'
 })
 export class AddProductService {
-  private http: HttpClient;
   public url : string = "http://localhost:9000/addProduct";
 
-  constructor(http: HttpClient) {
-    this.http = http;
+  constructor(private http: HttpClient) {
   }
 
   addProduct(x: NewProduct) {
