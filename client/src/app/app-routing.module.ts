1f11fc74ae6b77e12c4e1bc4599c8d15648fab19
Tom Dirinck <tom.dirinck@gmail.com>
2021-07-16 11:48:48 +0200

    Added routing to new create pages.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OrdersComponent } from './src/components/main/orders/orders.component';
 import { ProductsComponent } from './src/components/main/products/products.component';
+import { AddOrderComponent } from './src/components/main/add-order/add-order.component';
+import { NewProductComponent } from './src/components/main/new-product/new-product.component';
 
 const routes: Routes = [
   { path: 'orders', component: OrdersComponent },
   { path: 'products', component: ProductsComponent },
+  { path: 'orders/new-order', component: AddOrderComponent },
+  { path: 'products/new-product', component: NewProductComponent },
+  { path: '**', component: OrdersComponent },
 ];
 
 @NgModule({
